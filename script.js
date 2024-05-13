af339e06050d5d60445832468c90c8586b23340b
pinewood-uru <119970370+pinewood-uru@users.noreply.github.com>
2024-05-13 15:10:17 -0300

    lunes

---
+++ script.js
@@ -0,0 +1,64 @@
+// HTML
+
+let ctnejercicios = document.getElementById("ctn-ejercicios");
+
+const ejercicios = [
+    {
+        nombre: "Ejercicio 1",
+        url: "ejercicio1.html",
+        img: "img/numeros-01.png",
+        descripcion:'',
+    },
+    {
+        nombre: "Ejercicio 2",
+        url: "ejercicio2.html",
+        img: "img/numeros-02.png",
+        descripcion:'',
+    },
+    {
+        nombre: "Ejercicio 3",
+        url: "ejercicio3.html",
+        img: "img/numeros-03.png",
+        descripcion:'',
+    },
+    {
+        nombre: "Ejercicio 4",
+        url: "ejercicio4.html",
+        img: "img/numeros-04.png",
+        descripcion:'',
+    },
+    {
+        nombre: "Ejercicio 5",
+        url: "ejercicio5.html",
+        img: "img/numeros-05.png",
+        descripcion:'',
+    },
+    {
+        nombre: "Ejercicio 6",
+        url: "ejercicio6.html",
+        img: "img/numeros-06.png",
+        descripcion:'',
+    },
+    {
+        nombre: "Ejercicio 7",
+        url: "ejercicio7.html",
+        img: "img/numeros-07.png",
+        descripcion:'',
+    },
+    ];
+
+ejercicios.forEach((ejercicio) => {
+    console.log(ejercicio);
+    let formatocontenedor = `
+    <div class="card" style="width: 18rem;">
+  <img src="${ejercicio.img}" class="card-img-top" alt="">
+  <div class="card-body">
+    <h5 class="card-title">${ejercicio.nombre}</h5>
+    <p class="card-text">${ejercicio.descripcion}</p>
+    <a href="${ejercicio.url}" class="btn btn-primary">Ir a ejercicio</a>
+  </div>
+</div>`
+
+ctnejercicios.innerHTML += formatocontenedor;
+
+})
