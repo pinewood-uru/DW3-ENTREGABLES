5d327d43e6d4e42a58cd2489834281b204df19eb
pinewood-uru <119970370+pinewood-uru@users.noreply.github.com>
2024-05-18 11:33:09 -0300

    sabado

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ const ejercicios = [
     },
     {
         nombre: "Ejercicio 2",
-        url: "ejercicios/ejercicio1/ejercicio2.html",
+        url: "ejercicios/ejercicio2/ejercicio2.html",
         img: "img/numeros-02.png",
         descripcion:'',
     },
     {
         nombre: "Ejercicio 3",
-        url: "ejercicios/ejercicio1/ejercicio3.html",
+        url: "https://dw3langingavellanal.netlify.app",
         img: "img/numeros-03.png",
         descripcion:'',
     },
@@ -57,7 +57,7 @@ ejercicios.forEach((ejercicio) => {
   <div class="card-body">
     <h5 class="card-title primary-text">${ejercicio.nombre}</h5>
     <p class="card-text">${ejercicio.descripcion}</p>
-    <a href="${ejercicio.url}" class="btn btn-primary">Ir a ejercicio</a>
+    <a target="_blank" href="${ejercicio.url}" class="btn btn-primary">Ir a ejercicio</a>
   </div>
 </div></div>`
 
